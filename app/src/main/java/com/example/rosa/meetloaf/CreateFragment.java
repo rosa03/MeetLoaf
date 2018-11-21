c51a25b2e6a4c990bfa1d44754eed554263427a1
Rosa03 <Rosa03@users.noreply.github.com>
2018-11-21 17:12:56 +0000

    Added a form to the create meetings page

--- app/src/main/java/com/example/rosa/meetloaf/CreateFragment.java
+++ app/src/main/java/com/example/rosa/meetloaf/CreateFragment.java
@@ -1,23 +1,21 @@
 package com.example.rosa.meetloaf;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class CreateFragment extends Fragment {
-
+    private Button selectTime;
 
     public CreateFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
